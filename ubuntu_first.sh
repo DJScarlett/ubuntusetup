91380f60f9bac4b27667aba61f6c0a77c4d288e4
DJScarlett <ga_mintblancmanche@yahoo.co.jp>
2019-07-02 12:17:37 +0900

    OSアップデートを別に分ける

--- ubuntu_first.sh
+++ ubuntu_first.sh
@@ -23,8 +23,6 @@ sudo apt clean -y
 # パッケージインストール
 sudo apt install -y git gcc g++ make openssl zlib1g-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev vim build-essential curl wget aptitude update-manager-core update-manager
 
-#OSバージョンアップ
-sudo do-release-upgrade -y
 
 #再起動
 echo "reboot"
